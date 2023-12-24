ad59a44e688e16cbb4c787b928391c1a6fde9150
alexkor <axel.korinnyi@gmail.com>
2023-12-25 00:46:27 +0200

    добавлені функції автентифікації

---
+++ authentication_send.py
@@ -0,0 +1,32 @@
+import random
+from serpent_encode import serpent_en
+
+def auto_send(key):#отримує параметром ключ для шифрування
+    file=open('text.txt','r')#для копіювання тексту
+    text=file.read()#початковий текст
+    leng=len(text)
+
+    file_3=open('text_random.txt','w')#очищення файлу
+    file_3.close()
+
+
+    file_3 = open('text_random.txt', 'a')
+    random_number=generator(leng)
+    file_3.write(text)#запис тексту у третій файл
+    file_3.write(random_number)#запис рандомних значень у третій файл
+    file_3.close()
+
+    encode = serpent_en(key, 'text_random.txt')#отримання закодованого тексту
+    file2 = open('file_name_2.txt', 'w')#запис закодованого тексту
+    for i in range(len(encode)):
+        file2.write(encode[i])
+    file2.close()
+
+
+    return random_number#повернення зашифрованого тексту і рандомного числа
+
+def generator(len):
+    string=''
+    for i in range((len//16+1)*16-len):
+        string+=str(random.randint(0,9))
+    return string
--- main.py
+++ main.py
@@ -1,26 +1,25 @@
-from serpent_encode import serpent_en
-from serpent_decode import serpent_de
-from service_step import to_symbol
-
-
+from authentication_send import auto_send as a_s
+from authentication_recipient import auto_recp as a_r
 
 
 
 
 if __name__ == '__main__':
-    file_name="text.txt"
-    file_name2='file_name_2.txt'
-
+    file_name="text.txt"#основний текст
+    file_name_with_random='text_random.txt'#файл в якому записані значення + рандомне число
+    file_name2='file_name_2.txt'#в цьому файлі записаний зашифрований текст
     key='H9$%42Q-r9*sdkhd'
 
-    encode=serpent_en(key,file_name)
-    file2=open('file_name_2.txt', 'w')
-    for i in range(len(encode)):
-        file2.write(encode[i])
-    file2.close()
-
-    decode=serpent_de(key,file_name2)
-    text_final=''
-    for i in range(len(decode)):
-        text_final+=to_symbol(decode[i])
-    print(text_final)
+    random_number=a_s(key)#повертає рандомне значення
+    d=a_r(key,random_number)
+    x=len(random_number)#довжина добавленого рандомного значення
+    first_file=open(file_name,'r')#основний текст
+    third_file=open(file_name_with_random,'r')#випадковий текст
+    first=first_file.read()
+    third=third_file.read()
+    if first==third[:-x] and first+random_number==third:#можна створити помилку замінивши рандом на інше
+        print("all is right")
+    elif first!=third[:-x]:
+        print("текст неправильний")
+    elif first+random_number==third:
+        print('Випадкове число передано з помилкою')
