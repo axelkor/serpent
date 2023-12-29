9c3f49fbe5e25bf9b2b9205e846db254b5c95f5f
alexkor <axel.korinnyi@gmail.com>
2023-12-29 10:50:37 +0200

    Додавання вікна для подальших дій

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 from authentication_send import auto_send as a_s
 from authentication_recipient import auto_recp as a_r
+from tkinter import *
+
+from tkinter import Tk, Frame, Label
+
+window = Tk()
+window['bg'] = '#ffffff'
+window.title('Автентифікація')
+window.geometry('450x300')
+window.resizable(width=False, height=False)
+
+frame = Frame(window, bg='grey')
+frame.place(relheight=1, relwidth=1)
+
+label1 = Label(frame, text="Сторона А").grid(row=0, column=0,sticky="ew")
+label2 = Label(frame, text="Сторона B").grid(row=0, column=1, sticky="ew")
+label3 = Label(frame, text="Текст автентифікації",bg='grey').grid(row=1, column=0,sticky="ew")
+entry1=Entry(frame).grid(row=2, column=0,sticky="ew")#Введення тексту А
+label4 = Label(frame, text="Рандомне значення",bg='grey').grid(row=3, column=0,sticky="ew")
+entry2=Entry(frame).grid(row=4, column=0,sticky="ew")#Рандомне число
+button1=Button(frame,text='Заповнення всіх комірок',bg='grey').grid(row=5, column=0,sticky="ew")
+button1=Button(frame,text='Розшифрування значень',bg='grey').grid(row=5, column=1,sticky="ew")
+label5 = Label(frame, text="Розшифрований текст",bg='grey').grid(row=1, column=1,sticky="ew")
+label6 = Label(frame, text="Отримане значення",bg='grey').grid(row=3, column=1,sticky="ew")
+entry3=Entry(frame).grid(row=2, column=1,sticky="ew")#Введення тексту А
+entry4=Entry(frame).grid(row=4, column=1,sticky="ew")#Рандомне число
+
+
+label7 = Label(frame, text="Автентифікація").grid(row=6,column=0,columnspan=2,sticky='ew')#Цей віджет буде в кінці кінців
 
 
 
+frame.grid_columnconfigure(0,minsize=225)
+frame.grid_columnconfigure(1,minsize=225)
+
+window.mainloop()
 
 if __name__ == '__main__':
     file_name="text.txt"#основний текст
